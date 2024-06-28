e1741fcf86199c8bd89fe3802cc7b4882cec6f84
Rahuletto <rahulmarban@gmail.com>
2024-06-28 13:32:10 +0530

    chore: no auth

--- utils/wakatime.ts
+++ utils/wakatime.ts
@@ -4,10 +4,7 @@ export default async function Wakatime(user: string) {
   const url = `https://api.wakatime.com/api/v1/users/${encodeURIComponent(user)}/stats?is_including_today=true&range=all_time`;
 
   const response = await fetch(url, {
-    method: "GET",
-    headers: {
-      "Authorization": `Bearer ${process.env.WAKATIME_TOKEN} `,
-    },
+    method: "GET"
   });
 
   const data: RawWakaData = await response.json();
