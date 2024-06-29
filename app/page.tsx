7571618893efe3b9a400cc183d0ca2bcd723d4e5
Rahuletto <rahulmarban@gmail.com>
2024-06-29 19:42:10 +0530

    style: scale for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
   return (
     <>
       <Header />
-      <main className="flex min-h-screen flex-col items-center justify-between">
+      <main className="scale-100 md:scale-90 flex min-h-screen flex-col items-center justify-between">
         <Hero />
         <Config />
         <Builder />
